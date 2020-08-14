5dd37a72818881eb533f200b430979284438a01a
xushuangyu <xushuangyu@bytedance.com>
2020-08-14 10:55:34 +0800

    use tsc to compile ts file into js file.

---
+++ sandbox.js
@@ -0,0 +1,5 @@
+var character = 'becca becca';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) { return console.log(input); });
---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const character = 'becca';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach(input => console.log(input));
